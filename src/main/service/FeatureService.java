f2f4e639674a71336acd1e74538601b100be2749
geraldng1407 <g-e-r-a-l-d@hotmail.com>
2022-08-27 17:12:32 +0800

    Added Services

---
+++ src/main/service/FeatureService.java
@@ -0,0 +1,9 @@
+package com.bbtutorials.users.service;
+import java.util.*;
+
+import com.bbtutorials.users.model.*;
+public class FeatureService {
+    public List<Features> listFeatures(){
+        
+    }
+}
---
+++ src/main/service/SolutionService.java
@@ -0,0 +1,11 @@
+package com.bbtutorials.users.service;
+
+import java.util.*;
+import com.bbtutorials.users.model.*;
+
+public class SolutionService {
+    public List<Solution> listSolutions(){
+
+    }
+}
+
